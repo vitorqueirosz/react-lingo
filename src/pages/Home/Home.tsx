f25a82c8664cf876e0b433415bdacb1e00832542
vitorqueirosz <vitor.queiroz16@hotmail.com>
2022-05-06 13:15:07 -0300

    feat(pages): add Mascot component to Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,30 @@
-import { Button } from '@/components';
+import { Anchor } from '@/components';
+import { Mascot } from '@/assets/images/';
+
+type Language = 'en' | 'pt';
+
+const ROUTES = {
+  lesson: (language: Language) => `/lessons/${language}`,
+};
 
 export const Home = () => {
   return (
     <div className="bg-gray-100 flex h-screen w-full flex-col items-center ">
-      <div className="flex gap-4 mt-40">
+      <div className="relative flex gap-4 mt-40">
         <h1 className="text-6xl font-bold text-gray-700">Welcome to</h1>
         <h1 className="text-6xl font-bold text-green-500">React-Lingo</h1>
+
+        <Mascot className="absolute w-24 h-40 left-full bottom-0" />
       </div>
 
       <main className="flex flex-col items-center mt-16">
-        <h3 className="text-3xl font-bold text-gray-700">
+        <h3 className="text-3xl font-bold text-gray-600">
           Choose your language
         </h3>
 
         <div className="flex mt-4 gap-2">
-          <Button>English</Button>
-          <Button>Portuguese</Button>
+          <Anchor href={ROUTES.lesson('en')}>English</Anchor>
+          <Anchor href={ROUTES.lesson('pt')}>Portuguese</Anchor>
         </div>
       </main>
     </div>
