53083e6feebab68af918ba0116d00b060c90f1e8
vitorqueirosz <vitor.queiroz16@hotmail.com>
2022-05-07 13:07:58 -0300

    feat(pages/Lessons/data): add lessons data

---
+++ src/pages/Lessons/data/lessons.tsx
@@ -0,0 +1,72 @@
+import { Woman } from '@/assets/images';
+
+export const lessons = {
+  en: {
+    beginner: {
+      steps: [
+        {
+          type: 'listening',
+          title: 'Write this in English',
+          image: <Woman />,
+          words: ['woman', 'boy', 'I', 'A', 'man'],
+          sentence: 'Um homem',
+        },
+      ],
+    },
+    medium: {
+      steps: [
+        {
+          type: 'listening',
+          title: 'Listening',
+          words: [],
+          sentence: 'List',
+        },
+      ],
+    },
+    advanced: {
+      steps: [
+        {
+          type: 'listening',
+          title: 'Listening',
+          words: [],
+          sentence: 'List',
+        },
+      ],
+    },
+  },
+  pt: {
+    beginner: {
+      steps: [
+        {
+          type: 'listening',
+          title: 'Write this in English',
+          image: <Woman />,
+          words: ['woman', 'boy', 'I', 'A', 'man'],
+          sentence: 'Um homem',
+        },
+      ],
+    },
+    medium: {
+      steps: [
+        {
+          type: 'listening',
+          title: 'Write this in English',
+          image: <Woman />,
+          words: ['woman', 'boy', 'I', 'A', 'man'],
+          sentence: 'Um homem',
+        },
+      ],
+    },
+    advanced: {
+      steps: [
+        {
+          type: 'listening',
+          title: 'Write this in English',
+          image: <Woman />,
+          words: ['woman', 'boy', 'I', 'A', 'man'],
+          sentence: 'Um homem',
+        },
+      ],
+    },
+  },
+};
