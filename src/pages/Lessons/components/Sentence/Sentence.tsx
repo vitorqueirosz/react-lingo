dc20b797f587f45d3ad42cf4e795135a9cda4b21
vitorqueirosz <vitor.queiroz16@hotmail.com>
2022-05-09 19:39:30 -0300

    feat(pages/Lessons): add answeredList and dynamic positions on words

--- src/pages/Lessons/components/Sentence/Sentence.tsx
+++ src/pages/Lessons/components/Sentence/Sentence.tsx
@@ -1,4 +1,4 @@
-import { ImgHTMLAttributes } from 'react';
+import { ImgHTMLAttributes, useRef, useState } from 'react';
 
 export type SentenceProps = {
   title: string;
@@ -13,6 +13,36 @@ export const Sentence = ({
   words,
   sentence,
 }: SentenceProps) => {
+  const [answeredList, setAnsweredList] = useState<string[]>([]);
+
+  const wordRef = useRef<HTMLDivElement | null>(null);
+  const answeredListRef = useRef<HTMLDivElement | null>(null);
+
+  const handleSelectWord = (id: string) => {
+    const exists = answeredList.includes(id);
+
+    if (exists) {
+      setAnsweredList((prevState) => prevState.filter((word) => word !== id));
+      wordRef.current!.style.transform = `translateX(0px) translateY(0px)`;
+
+      return;
+    }
+
+    const wordBoundingLeft = wordRef.current!.getBoundingClientRect().left;
+    const listBoundingLeft =
+      answeredListRef.current!.getBoundingClientRect().left;
+
+    const left = wordBoundingLeft - listBoundingLeft;
+    const top =
+      wordRef.current!.offsetTop - answeredListRef.current!.offsetTop - 10;
+
+    wordRef.current!.style.transform = `translateX(-${left}px) translateY(-${top}px)`;
+
+    setTimeout(() => {
+      setAnsweredList((prevState) => [...prevState, id]);
+    }, 300);
+  };
+
   return (
     <div className="flex justify-center flex-col w-full max-w-screen-sm h-full">
       <h1 className="font-bold text-3xl text-slate-600">{title}</h1>
@@ -27,19 +57,53 @@ export const Sentence = ({
         </div>
       </div>
 
-      <div className="h-16 w-full border-y-2 border-neutral-200">test</div>
+      <div>
+        <div
+          ref={answeredListRef}
+          className="flex flex-wrap items-center h-20 w-full border-y-2 border-neutral-200"
+        >
+          {answeredList.map((word) => (
+            <div
+              key={word}
+              className="border-2 border-b-4 border-neutral-200 rounded-xl mr-1"
+              style={{
+                transition: 'all 0.5s ease-in-out',
+                height: '60px',
+              }}
+            >
+              <button
+                className="p-4"
+                type="button"
+                onClick={() => handleSelectWord(word)}
+              >
+                {word}
+              </button>
+            </div>
+          ))}
+        </div>
 
-      <div className="flex items-center justify-center w-full mt-12">
-        {words.map((word) => (
-          <div
-            key={word}
-            className="border-2 border-b-4 border-neutral-200 rounded-xl mr-2"
-          >
-            <button className="p-4" type="button">
-              {word}
-            </button>
-          </div>
-        ))}
+        <div className="flex items-center justify-center w-full mt-12">
+          {words.map((word) => (
+            <div key={word} className="bg-gray-200 rounded-xl z-0 mr-1">
+              <div
+                className="border-2 z-10 bg-white relative border-b-4 border-neutral-200 rounded-xl "
+                ref={wordRef}
+                style={{
+                  transition: 'all 0.3s ease-in-out',
+                  height: '60px',
+                }}
+              >
+                <button
+                  className="p-4"
+                  type="button"
+                  onClick={() => handleSelectWord(word)}
+                >
+                  {word}
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
