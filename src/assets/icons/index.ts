a42f9b028cf5e6501bc1895eb5ec18379933fa8b
vitorqueirosz <vitor.queiroz16@hotmail.com>
2022-05-11 17:10:28 -0300

    feat(assets/icons): add border-close and check Icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,5 @@
 export { ReactComponent as IcChevronLeft } from './chevron-left.svg';
 export { ReactComponent as IcClose } from './close.svg';
 export { ReactComponent as IcSound } from './sound.svg';
+export { ReactComponent as IcCheck } from './check.svg';
+export { ReactComponent as IcBorderClose } from './border-close.svg';
