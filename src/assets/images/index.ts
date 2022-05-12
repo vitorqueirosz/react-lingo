90bc6c6e4c3d25c62583a48046f7a996ee00f8d3
vitorqueirosz <vitor.queiroz16@hotmail.com>
2022-05-11 21:41:52 -0300

    feat(assets/images): add Images]=

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,2 +1,5 @@
 export { ReactComponent as Mascot } from './mascot.svg';
 export { ReactComponent as Woman } from './woman.svg';
+export { ReactComponent as Boy } from './boy.svg';
+export { ReactComponent as Man } from './man.svg';
+export { ReactComponent as WomanSmile } from './woman-smile.svg';
