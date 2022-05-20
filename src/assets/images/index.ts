28737731a244b5b3563ae92c7ac26e03af7158d1
vitorqueirosz <vitor.queiroz16@hotmail.com>
2022-05-20 15:57:41 -0300

    feat(assets/images): add Images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,3 +3,7 @@ export { ReactComponent as Woman } from './woman.svg';
 export { ReactComponent as Boy } from './boy.svg';
 export { ReactComponent as Man } from './man.svg';
 export { ReactComponent as WomanSmile } from './woman-smile.svg';
+export { ReactComponent as Fireman } from './fireman.svg';
+export { ReactComponent as Police } from './police.svg';
+export { ReactComponent as SecurityMan } from './security-man.svg';
+export { ReactComponent as ManSmile } from './man-smile.svg';
