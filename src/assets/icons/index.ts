f60ff4f51ff75d2051810eb656575f9492c3294b
vitorqueirosz <vitor.queiroz16@hotmail.com>
2022-05-19 22:31:03 -0300

    feat(assets/icons): add Reload icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,3 +4,4 @@ export { ReactComponent as IcSound } from './sound.svg';
 export { ReactComponent as IcCheck } from './check.svg';
 export { ReactComponent as IcBorderClose } from './border-close.svg';
 export { ReactComponent as IcSlowSound } from './slow-sound.svg';
+export { ReactComponent as IcReload } from './reload.svg';
